fafb4c363328b7b67a21964eea006873e787ea03
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-03-23 00:15:03 +0100

    Back end complet / Front end juste setup

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log("MONGODB SUCCESS CONNECT !")
+
+    } catch (error) {
+        console.error("Error connecting to mongodb", error);
+        process.exit(1);
+    }
+}
---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 5001
+
+connectDB();
+
+
+//middleware
+app.use(express.json())
+app.use("/api/notes", notesRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log("Server started on PORT :" + PORT);
+});
