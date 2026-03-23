ade073573e73c6e62ec8c5b812d448c2d806b5bb
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-03-23 01:04:25 +0100

    Mise en place de la navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header className="bg-base-300 border-b border-base-content/10">
+        <div className="mx-auto max-w-6xl p-4">
+            <div className="flex items-center justify-between"> 
+                <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
+                    Intant-Quote v0.1</h1>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar.jsx'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="min-h-screen">
+        <Navbar></Navbar>
+    </div>
   )
 }
 
