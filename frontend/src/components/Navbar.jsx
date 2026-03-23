d1145c9088f8f9b2e214b3e6ed54d338f7b9a1da
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-03-23 02:31:53 +0100

    Test de la librairie Lucide pour les icones

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Home, Folder } from 'lucide-react'
+
 
 function Navbar() {
   return (
     <header className="bg-base-300 border-b border-base-content/10">
         <div className="mx-auto max-w-6xl p-4">
             <div className="flex items-center justify-between"> 
-                <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
+                <h1 className="text-2xl font-bold text-primary font-mono tracking-tight">
                     Intant-Quote v0.1</h1>
+                <div className="flex-row">
+                    <Home size ={24}/>
+                    <Folder size={24}/>
+                </div>
             </div>
         </div>
     </header>
