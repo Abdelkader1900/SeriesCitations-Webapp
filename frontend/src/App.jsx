1271eea8c6910c5c1a2e527e8e7a66946c9e73e0
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-03-24 17:39:16 +0100

    Possible de créer des citations perssistantes back/front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import { Toaster } from 'react-hot-toast'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
@@ -7,6 +8,7 @@ import NoteDetailPage from './pages/NoteDetailPage'
 const App = () => {
   return (
     <div className='absolute inset-0 -z-10 h-full w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]'>
+      <Toaster />
       <Routes>
         <Route path ="/" element ={<HomePage/>}/>
         <Route path ="/note/:id" element ={<NoteDetailPage/>}/>
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,16 +1,40 @@
 import { ArrowLeftIcon } from 'lucide-react';
 import React from 'react'
 import { useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate, useNavigate } from 'react-router';
+import { toast } from "react-hot-toast";
+import axios from 'axios';
 
 function CreatePage() {
     const [title,setTitle] = useState("");
     const [content, setContent] = useState("");
     const [loading,setLoading] = useState(false)
+    const navigate = useNavigate()
 
-    const handleSubmit = () => {
+    const handleSubmit = async (e) => {
+      e.preventDefault();
       console.log(title);
       console.log(content);
+      
+      if( !title.trim() || !content.trim()){
+        toast.error("Tous les champs sont requis !")
+        return
+      }
+
+      setLoading(true)
+      try{
+        await axios.post("http://localhost:5001/api/notes",{
+          title,
+          content
+        })
+        toast.success("La citation a été ajoutée a votre bibliothèque")
+        navigate("/")
+      }catch(error){
+        toast.error("Impossible d'ajouter la citation.")
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
 
     }
 
@@ -24,19 +48,15 @@ function CreatePage() {
           </Link>
           <div className='card bg-base-100'>
             <div className='card-body'>
-              <h2 className='card-title text-2xl mb-4'>Create New Note</h2>
+              <h2 className='card-title text-2xl mb-4'>Add a New Quote</h2>
               <form onSubmit={handleSubmit}>
-                <div className='form-control mb-4'>
-                  <label className='label'>
-                    <span className='label-text mx-3'> Title</span>
-                  </label>
+                <div className='flex flex-col mb-4'>
+                  <label className='label-text mb-1'>Title</label>
                   <input type="text" placeholder='Quote Title' className='input input-bordered' value={title} onChange={(e) => setTitle(e.target.value)}  />
                 </div>
 
-                <div className='form-control mb-4'>
-                  <label className='label'>
-                    <span className='label-text mx-3'> Content</span>
-                  </label>
+                <div className='flex flex-col mb-4'>
+                  <label className='label-text mb-1'>Content</label>
                   <textarea placeholder='Quote' className='textarea textarea-bordered h-36 w-100 resize-none' value={content} onChange={(e) => setContent(e.target.value)}  />
                 </div>
 
